d86ba4ade2fdfc9351a0c7fed16f2aa29390461f
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2025-02-08 09:58:52 -0600

    remove paypal

--- src/routes/paypal-checkout/[totalPrice]/+page.server.js
+++
@@ -1,9 +0,0 @@
-export const load = async (event) => {
-	if (!event.locals?.user) {
-		return { customerId: null };
-	}
-	const customerId = event.locals.user?.googleId;
-	return {
-		customerId
-	};
-};
